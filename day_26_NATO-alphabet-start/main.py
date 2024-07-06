edba4514ae9750c636221eeb184d2b698b5eeb1d
Krishna Kumar <145002440+krishnakumar9957@users.noreply.github.com>
2024-07-06 12:58:22 +0530

    Add files via upload

---
+++ day_26_NATO-alphabet-start/main.py
@@ -0,0 +1,37 @@
+student_dict = {
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
+}
+
+#Looping through dictionaries:
+for (key, value) in student_dict.items():
+    #Access key and value
+    pass
+
+import pandas
+student_data_frame = pandas.DataFrame(student_dict)
+
+#Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    #Access index and row
+    #Access row.student or row.score
+    pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+
+
+
+alpha_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+def input_word():
+    word = input("enter your name: ").upper()
+    try:
+        my_list = [alpha_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry only alphabet is valid for this operation")
+        input_word()
+    else:
+        print(my_list)
+input_word()
